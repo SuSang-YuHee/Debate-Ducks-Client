e496b348833d17627582aca5fe432aaae7441468
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-11 02:09:23 +0900

    [Modify] 단순 함수 합치기

--- components/debateroom/utils/webSocket.ts
+++ components/debateroom/utils/webSocket.ts
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 import Peer from "simple-peer";
 
-import { connectHostPeer, connectGuestPeer } from "./simple-peer";
 import { drawNotice } from "./draw";
 import { beep } from "./beep";
 import { offScreenShare } from "./screenShare";
@@ -227,3 +226,102 @@ export const wsTransmitSkip = ({
 }: Pick<IDebateroom, "debateId" | "socket" | "isPros">) => {
   socket.current?.emit("skip", { debateId, isPros });
 };
+
+function connectHostPeer({
+  debateId,
+  socket,
+  peerRef,
+  stream,
+  setPeerStream,
+  peerVideoRef,
+}: Pick<
+  IDebateroom,
+  | "debateId"
+  | "socket"
+  | "peerRef"
+  | "stream"
+  | "setPeerStream"
+  | "peerVideoRef"
+>) {
+  const simplePeer = new Peer({
+    initiator: true,
+    trickle: false,
+    config: {
+      iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.nextcloud.com:443" },
+      ],
+    },
+    stream,
+  });
+
+  peerRef.current = simplePeer;
+
+  simplePeer.on("signal", (signal) => {
+    socket.current?.emit("offer", { debateId, signal });
+  });
+
+  simplePeer.on("stream", (stream) => {
+    setPeerStream(stream);
+    if (peerVideoRef.current) {
+      peerVideoRef.current.srcObject = stream;
+    }
+  });
+
+  simplePeer.on("error", (err) => {
+    console.log(err);
+  });
+
+  socket.current?.on("answer", (signal: Peer.SignalData) => {
+    simplePeer.signal(signal);
+  });
+}
+
+function connectGuestPeer(
+  {
+    debateId,
+    socket,
+    peerRef,
+    stream,
+    setPeerStream,
+    peerVideoRef,
+  }: Pick<
+    IDebateroom,
+    | "debateId"
+    | "socket"
+    | "peerRef"
+    | "stream"
+    | "setPeerStream"
+    | "peerVideoRef"
+  >,
+  signal: Peer.SignalData,
+) {
+  const simplePeer = new Peer({
+    initiator: false,
+    trickle: false,
+    stream,
+  });
+
+  peerRef.current = simplePeer;
+
+  simplePeer.on("signal", (signal) => {
+    socket.current?.emit("answer", { debateId, signal });
+  });
+
+  simplePeer.on("stream", (stream) => {
+    setPeerStream(stream);
+    if (peerVideoRef.current) {
+      peerVideoRef.current.srcObject = stream;
+    }
+  });
+
+  simplePeer.on("error", (err) => {
+    console.log(err);
+  });
+
+  simplePeer.signal(signal);
+}
