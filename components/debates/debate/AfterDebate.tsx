2cfaca791ce2ef286a7cbff5d00fdbf6777828f4
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-24 19:04:02 +0900

    [Fix] video_url Get 요청 문제 임시 해결 (주석 처리)

--- components/debates/debate/AfterDebate.tsx
+++ components/debates/debate/AfterDebate.tsx
@@ -9,7 +9,9 @@ export default function AfterDebate({ debateId }: { debateId: number }) {
     <div>
       {debate.data?.video_url ? (
         <div>
-          <video src={debate.data?.video_url} width="0" height="0"></video>
+          {/* <video width="0" height="0" controls preload="auto">
+            <source src={debate.data?.video_url} type="video/webm"></source>
+          </video> */}
           <p>팩트체크</p>
           <Vote debateId={debateId} />
         </div>
