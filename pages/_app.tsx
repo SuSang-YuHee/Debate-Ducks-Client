20aafd8e5107d104839f3c2ab16a1302519a569c
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-28 16:20:25 +0900

    [Setting] 초기 스타일, 메타 정보 및 파비콘 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Toaster } from "react-hot-toast";
+import "../styles/globals.scss";
+import Head from "next/head";
 
 import Header from "../components/Header";
 
@@ -28,6 +30,18 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>Debate Ducks</title>
+          <meta name="author" content="SuSang-YuHee" />
+          <meta name="description" content="Real-time debating platform." />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="./favicon.ico" />
+          <link rel="apple-touch-icon" href="./favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css"
+          />
+        </Head>
         {isToaster ? (
           <Toaster
             toastOptions={{
