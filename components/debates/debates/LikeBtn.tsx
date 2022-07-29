dfc77768694937c396f9dc52f9861ab343305838
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-29 18:13:23 +0900

    [Change] 로그인이 필요한 모달 적용

--- components/debates/debates/LikeBtn.tsx
+++ components/debates/debates/LikeBtn.tsx
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
 import { Dispatch, SetStateAction, useState } from "react";
 
 import { useGetUser } from "../../../utils/queries/users";
-import ConfirmModal from "../../common/modal/ConfirmModal";
 import styles from "./LikeBtn.module.scss";
+
+import CheckSignInModal from "../../common/modal/CheckSignInModal";
 
 export default function LikeBtn({
   isHeartListOn,
@@ -12,25 +12,13 @@ export default function LikeBtn({
   isHeartListOn: boolean;
   setIsHeartListOn: Dispatch<SetStateAction<boolean>>;
 }) {
-  const router = useRouter();
   const [isModalOn, setIsModalOn] = useState<boolean>(false);
 
   const user = useGetUser();
 
   return (
     <>
-      {isModalOn ? (
-        <ConfirmModal
-          title={`로그인이 필요한 서비스입니다.`}
-          content={`로그인을 하고 서비스를 이용하시겠습니까?`}
-          firstBtn={"아니요"}
-          firstFunc={() => {
-            setIsModalOn(false);
-          }}
-          secondBtn={"로그인하기"}
-          secondFunc={() => router.push("/signin")}
-        />
-      ) : null}
+      <CheckSignInModal isModalOn={isModalOn} setIsModalOn={setIsModalOn} />
       <div className={styles.outer}>
         {isHeartListOn ? (
           <div
