da9cc5b07a762027bf12f2ea7766d39906d7c86d
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-29 23:23:11 +0900

    [Update] DebateContainer 개선

--- components/debates/debates/DebatesContainer.tsx
+++ components/debates/debates/DebatesContainer.tsx
@@ -3,25 +3,30 @@ import { UseInfiniteQueryResult } from "react-query";
 import { useRouter } from "next/router";
 import { useInView } from "react-intersection-observer";
 
-import { checkCategory } from "../../../utils/debates/checkCategory";
-// import { useGetUser } from "../../../utils/queries/users";
+import { checkFilters } from "../../../utils/debates/checkFilters";
+import { useGetUser } from "../../../utils/queries/users";
+import { COMMENT_ORDER, STATUSES } from "../../../utils/common/constant";
+import styles from "./DebatesContainer.module.scss";
+
+import CheckSignInModal from "../../common/modal/CheckSignInModal";
 
 import {
   DebateOfDebates,
   UseInputResult,
   UseSelectResult,
 } from "../../../types";
-import { COMMENT_ORDER } from "../../../utils/common/constant";
 
 export default function DebatesContainer({
-  list,
+  statuses,
+  categories,
   isDisabledSearch,
   debates,
   refetch,
   search,
   orderSelect,
 }: {
-  list: string[];
+  statuses: string[];
+  categories: string[];
   isDisabledSearch: boolean;
   debates: UseInfiniteQueryResult<
     {
@@ -37,80 +42,121 @@ export default function DebatesContainer({
 }) {
   const router = useRouter();
   const { ref, inView } = useInView();
+  const [isModalOn, setIsModalOn] = useState<boolean>(false);
   const [isSearchListOn, setIsSearchListOn] = useState<boolean>(false);
 
-  // const user = useGetUser();
-
-  const checkEmpty = () => {
-    return (
-      debates.data?.pages
-        .map((page) =>
-          page.list.filter((debate: DebateOfDebates) =>
-            checkCategory(list, debate.category),
-          ),
-        )
-        .flat().length === 0
-    );
-  };
+  const user = useGetUser();
 
   useEffect(() => {
     if (inView && debates.hasNextPage) debates.fetchNextPage();
   }, [debates, inView]);
 
+  const checkEmpty = () => {
+    return (
+      debates.data?.pages
+        .map((page) =>
+          page.list.filter((debate: DebateOfDebates) => {
+            const status = debate.video_url
+              ? STATUSES[2]
+              : debate.participant?.id
+              ? STATUSES[1]
+              : STATUSES[0];
+            return checkFilters(statuses, status, categories, debate.category);
+          }),
+        )
+        .flat().length === 0
+    );
+  };
+
   return (
-    <div>
-      <div>
-        <button onClick={() => router.push("/debates/create")}>
+    <>
+      <CheckSignInModal isModalOn={isModalOn} setIsModalOn={setIsModalOn} />
+      <div className={styles.outer}>
+        <div
+          className={styles.btn_edit}
+          onClick={() => {
+            if (user.data) {
+              router.push("/debates/create");
+            } else {
+              setIsModalOn(true);
+            }
+          }}
+        >
           토론 만들기
-        </button>
-        {isSearchListOn ? (
-          <div>
-            <input {...search.attribute} disabled />
-            <button
-              onClick={() => {
-                search.setValue("");
-                refetch();
-                setIsSearchListOn(false);
-              }}
-            >
-              X
-            </button>
-          </div>
-        ) : (
-          <div>
-            <input {...search.attribute} disabled={isDisabledSearch} />
-            <button
-              onClick={() => {
-                refetch();
-                setIsSearchListOn(true);
-              }}
-              disabled={!search.value || isDisabledSearch}
-            >
-              검색
-            </button>
-          </div>
-        )}
-      </div>
-      <select {...orderSelect.attribute}>
-        {COMMENT_ORDER.map((order) => (
-          <option key={order[0]} value={order[1]}>
-            {order[0]}
-          </option>
-        ))}
-      </select>
-      {debates.data?.pages.map((page, idx) => (
-        <div key={idx}>
-          {page.list.map((debate: DebateOfDebates) =>
-            checkCategory(list, debate.category) ? (
-              <div key={debate.id}>
-                <div>{debate.title}</div>
+        </div>
+        <div className={styles.container}>
+          <select className={styles.select} {...orderSelect.attribute}>
+            {COMMENT_ORDER.map((order) => (
+              <option key={order[0]} value={order[1]}>
+                {order[0]}
+              </option>
+            ))}
+          </select>
+          {isSearchListOn ? (
+            <div className={styles.item}>
+              <input className={styles.input} {...search.attribute} disabled />
+              <div
+                className={styles.btn_search}
+                onClick={() => {
+                  search.setValue("");
+                  refetch();
+                  setIsSearchListOn(false);
+                }}
+              >
+                x
               </div>
-            ) : null,
+            </div>
+          ) : (
+            <div className={styles.item}>
+              <input
+                className={styles.input}
+                {...search.attribute}
+                disabled={isDisabledSearch}
+              />
+              <div
+                className={`${styles.btn_search} ${
+                  search.value && !isDisabledSearch
+                    ? ""
+                    : styles.btn_search_disabled
+                }`}
+                onClick={() => {
+                  if (search.value && !isDisabledSearch) {
+                    refetch();
+                    setIsSearchListOn(true);
+                  }
+                }}
+              >
+                v
+              </div>
+            </div>
           )}
         </div>
-      ))}
-      <div>{checkEmpty() ? "토론이 없습니다." : null}</div>
-      <div ref={ref}></div>
-    </div>
+        <div className={styles.cards}>
+          {debates.data?.pages.map((page, idx) => (
+            <div key={idx}>
+              {page.list.map((debate: DebateOfDebates) => {
+                const status = debate.video_url
+                  ? STATUSES[2]
+                  : debate.participant?.id
+                  ? STATUSES[1]
+                  : STATUSES[0];
+                return checkFilters(
+                  statuses,
+                  status,
+                  categories,
+                  debate.category,
+                ) ? (
+                  <div key={debate.id}>
+                    <div>{`${debate.title} / ${debate.video_url} / ${debate.participant}`}</div>
+                  </div>
+                ) : null;
+              })}
+            </div>
+          ))}
+          <div>{checkEmpty() ? "토론이 없습니다." : null}</div>
+          <div ref={ref}></div>
+        </div>
+      </div>
+    </>
   );
 }
