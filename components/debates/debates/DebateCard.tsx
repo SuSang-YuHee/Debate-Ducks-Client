c066b4200a2321e90d516971573828f88a3628ce
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-07-30 20:41:46 +0900

    [Update] 토론 카드 완성

--- components/debates/debates/DebateCard.tsx
+++ components/debates/debates/DebateCard.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-import { CATEGORIES } from "../../../utils/common/constant";
+import { CATEGORIES, STATUSES } from "../../../utils/common/constant";
+import { DMYorHM } from "../../../utils/common/formatStrDate";
 import styles from "./DebateCard.module.scss";
 
 import Debater from "./Debater";
@@ -14,11 +16,23 @@ export default function DebateCard({
   debate: DebateOfDebates;
   status: string;
 }) {
-  console.log(status);
+  const router = useRouter();
 
   return (
-    <div className={styles.card}>
+    <div
+      className={styles.card}
+      onClick={() => router.push(`/debates/${debate.id}`)}
+    >
       <div className={styles.category}>
+        <div
+          className={`${styles.status} ${
+            status === STATUSES[0] ? styles.status_0 : ""
+          } ${status === STATUSES[1] ? styles.status_1 : ""} ${
+            status === STATUSES[2] ? styles.status_2 : ""
+          }`}
+        >
+          {status}
+        </div>
         <Image
           className={styles.image}
           src={`/images/categories/${CATEGORIES.indexOf(debate.category)}.jpg`}
@@ -39,6 +53,11 @@ export default function DebateCard({
           isAuthorPros={!debate.author_pros}
           isPros={false}
         />
+      </div>
+      <div className={styles.title}>{debate.title}</div>
+      <div className={styles.box}>
+        <div className={styles.box_heart}>♥︎ {debate.hearts_cnt}</div>
+        <div className={styles.box_date}>{DMYorHM(debate.created_date)}</div>
       </div>
     </div>
   );
