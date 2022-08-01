377999a26f86ca47c5f7cb4bf38582e67e16247f
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-08-02 04:53:41 +0900

    [Update] 팩트체크 스타일 적용 및 디테일 추가

--- components/debates/debate/Factchecks.tsx
+++ components/debates/debate/Factchecks.tsx
@@ -1,117 +1,110 @@
-import { useState } from "react";
+import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
 
-import { useGetDebate } from "../../../utils/queries/debates";
-import {
-  useDeleteFactcheck,
-  usePatchFactcheck,
-  usePostFactcheck,
-} from "../../../utils/queries/factchecks";
+import { usePostFactcheck } from "../../../utils/queries/factchecks";
 import { useInput } from "../../../utils/common/useInputSelect";
+import { useGetUser } from "../../../utils/queries/users";
+import { useGetDebate } from "../../../utils/queries/debates";
+import { removeSpace } from "../../../utils/common/removeSpace";
+import styles from "./Factchecks.module.scss";
 
-import ConfirmModal from "../../common/modal/ConfirmModal";
+import Factcheck from "./Factcheck";
 
 export default function Factchecks({ debateId }: { debateId: number }) {
-  const [isEditOn, setIsEditOn] = useState<boolean>(false);
-  const [isDeleteModalOn, setIsDeleteModalOn] = useState<boolean>(false);
-  const [factcheckId, setFactcheckId] = useState<number>(0);
+  const [isCreateOn, setIsCreateOn] = useState<boolean>(false);
+  const [cnt, setCnt] = useState<number>(0);
 
+  const descriptionCreateInput = useInput("", "");
+  const referenceCreateInput = useInput("", "");
+
+  const user = useGetUser();
   const debate = useGetDebate(debateId);
   const postFactcheck = usePostFactcheck(debateId);
-  const patchFactcheck = usePatchFactcheck(debateId);
-  const deleteFactcheck = useDeleteFactcheck(debateId);
 
-  const referenceCreateInput = useInput("", "");
-  const descriptionCreateInput = useInput("", "");
-  const referenceEditInput = useInput("", "");
-  const descriptionEdithInput = useInput("", "");
+  useEffect(() => {
+    setCnt(removeSpace(descriptionCreateInput.value).length);
+  }, [descriptionCreateInput.value]);
+
+  const handleCreate = () => {
+    const description = removeSpace(descriptionCreateInput.value);
+    const reference_url = removeSpace(referenceCreateInput.value);
+
+    if (reference_url.length > 0 && !/^http[s]?\:\/\//i.test(reference_url)) {
+      toast.error("참조 주소는 url 형태여야 합니다.");
+    } else if (description.length < 5 || description.length > 500) {
+      toast.error("팩트체크 내용은 5자 이상, 500자 이하여야 합니다.");
+    } else {
+      let isPros;
+      if (debate.data?.author_pros) {
+        if (debate.data?.author?.id === user.data?.id) isPros = true;
+        if (debate.data?.participant?.id === user.data?.id) isPros = false;
+      } else {
+        if (debate.data?.author?.id === user.data?.id) isPros = false;
+        if (debate.data?.participant?.id === user.data?.id) isPros = true;
+      }
+
+      if (typeof isPros === "boolean") {
+        postFactcheck.mutate({
+          target_debate_id: debateId,
+          target_user_id: user.data?.id || "",
+          pros: isPros,
+          description,
+          reference_url,
+        });
+        referenceCreateInput.setValue("");
+        descriptionCreateInput.setValue("");
+      }
+    }
+  };
 
   return (
     <>
-      {isDeleteModalOn ? (
-        <ConfirmModal
-          title={"삭제 확인"}
-          content={"게시물을 삭제하시겠습니까?"}
-          firstBtn={"취소하기"}
-          firstFunc={() => {
-            setIsDeleteModalOn(false);
-          }}
-          secondBtn={"삭제하기"}
-          secondFunc={() => {
-            deleteFactcheck.mutate(factcheckId);
-            setIsDeleteModalOn(false);
-          }}
-        />
-      ) : null}
-      {debate.data?.factchecks
-        .filter((factcheck) => factcheck.pros)
-        .map((factcheck) => (
-          <div key={factcheck.id}>
-            {isEditOn && factcheckId === factcheck.id ? (
-              <div>
-                {"참조: "}
-                <input {...descriptionEdithInput.attribute} />
-                {"주소: "}
-                <input {...referenceEditInput.attribute} />
-                <button onClick={() => setIsEditOn(false)}>취소</button>
-                <button
-                  onClick={() => {
-                    patchFactcheck.mutate({
-                      description: descriptionEdithInput.value,
-                      reference_url: referenceEditInput.value,
-                      id: factcheck.id,
-                    });
-                    setIsEditOn(false);
-                  }}
-                >
-                  수정
-                </button>
-              </div>
-            ) : (
-              <div>
-                <p>{factcheck.description}</p>
-                <a href={factcheck.reference_url}>{factcheck.reference_url}</a>
-                <button
-                  onClick={() => {
-                    setFactcheckId(factcheck.id);
-                    descriptionEdithInput.setValue(factcheck.description);
-                    referenceEditInput.setValue(factcheck.reference_url);
-                    setIsEditOn(true);
-                  }}
-                >
-                  수정
-                </button>
-              </div>
-            )}
+      <div className={styles.factchecks_outer}>
+        <Factcheck debateId={debateId} isPros={true} />
+        <Factcheck debateId={debateId} isPros={false} />
+      </div>
+
+      {isCreateOn ? (
+        <div className={styles.create_factcheck}>
+          <div className={styles.name}>팩트체크 내용</div>
+          <textarea
+            className={styles.create_input}
+            {...descriptionCreateInput.attribute}
+          />
+          <div
+            className={`${styles.cnt} ${
+              cnt >= 5 && cnt <= 500 ? styles.cnt_active : ""
+            }`}
+          >
+            {cnt} / 500
+          </div>
+          <div className={styles.name}>참조 주소</div>
+          <input
+            className={styles.create_input}
+            {...referenceCreateInput.attribute}
+          />
+          <div className={styles.btn_box}>
             <button
-              onClick={() => {
-                setFactcheckId(factcheck.id);
-                setIsDeleteModalOn(true);
-              }}
+              className={`${styles.btn_small} ${styles.btn_small_cons}`}
+              onClick={() => setIsCreateOn(false)}
             >
-              삭제
+              닫기
+            </button>
+            <button
+              className={`${styles.btn_small} ${styles.btn_small_pros}`}
+              onClick={handleCreate}
+            >
+              작성
             </button>
           </div>
-        ))}
-      {debate.data?.factchecks.filter((factcheck) => !factcheck.pros)}
-      {"참조: "}
-      <input {...descriptionCreateInput.attribute} />
-      {"주소: "}
-      <input {...referenceCreateInput.attribute} />
-      <button
-        onClick={() => {
-          postFactcheck.mutate({
-            target_debate_id: debateId,
-            target_user_id: "01G85SA6V8NXD7XGB155SC4S18",
-            pros: true,
-            description: descriptionCreateInput.value,
-            reference_url: referenceCreateInput.value,
-          });
-          referenceCreateInput.setValue("");
-          descriptionCreateInput.setValue("");
-        }}
-      >
-        작성
-      </button>
+        </div>
+      ) : user.data &&
+        (user.data.id === debate.data?.author?.id ||
+          user.data.id === debate.data?.participant?.id) ? (
+        <button className={styles.btn_big} onClick={() => setIsCreateOn(true)}>
+          팩트체크 작성
+        </button>
+      ) : null}
     </>
   );
 }
