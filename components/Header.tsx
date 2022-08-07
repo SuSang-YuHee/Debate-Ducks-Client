f2497b98d803ff53d059f3345e5d929c3e3bb65d
strawberryoolongtea <strawberrycream@kakao.com>
2022-08-07 19:36:22 +0900

    [Update] 헤더 수정 - 헤더 이너 너비 수정 - 마이페이지 모달 취소 (여백 선택시 모달창 닫기) - 로그아웃 기능 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,61 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { useGetUser } from "../utils/queries/users";
+import ConfirmModal from "./common/modal/ConfirmModal";
 import styles from "./Header.module.scss";
 
 export default function Header() {
   const user = useGetUser();
   const router = useRouter();
   const [isSigninModalOpen, setIsSigninModalOpen] = useState(false);
+  const [isSignoutModalOpen, setIsSignoutModalOpen] = useState(false);
+
   const handleLogoClick = () => {
     router.push("/");
   };
+
   const handleProfileClick = () => {
-    // router.push("/mypage");
     setIsSigninModalOpen(!isSigninModalOpen);
   };
+
   const handleSigninBtnClick = () => {
     router.push("/signin");
   };
+
   const handleMyPageClick = () => {
     router.push("/mypage");
+    setIsSigninModalOpen(false);
   };
+
   return (
     <div className={styles.container}>
+      {isSignoutModalOpen ? (
+        <ConfirmModal
+          title="다음에 또 봐요! 👋"
+          content="로그아웃 하시겠습니까?"
+          firstBtn="머무르기"
+          firstFunc={() => {
+            setIsSignoutModalOpen(false);
+          }}
+          secondBtn={"로그아웃"}
+          secondFunc={() => {
+            setIsSignoutModalOpen(false);
+            window.localStorage.removeItem("debate-ducks-token");
+            router.push("/");
+            toast.success("로그아웃 되었습니다!");
+          }}
+        />
+      ) : null}
+      {isSigninModalOpen ? (
+        <div
+          className={styles.background}
+          onClick={() => {
+            setIsSigninModalOpen(false);
+          }}
+        ></div>
+      ) : null}
       <div className={styles.logo_container} onClick={handleLogoClick}>
         <Image
           src="/images/logo/debate-ducks-symbol.svg"
@@ -38,7 +71,15 @@ export default function Header() {
             <li className={styles.item} onClick={handleMyPageClick}>
               마이페이지
             </li>
-            <li className={styles.item}>로그아웃</li>
+            <li
+              className={styles.item}
+              onClick={() => {
+                setIsSignoutModalOpen(true);
+                setIsSigninModalOpen(false);
+              }}
+            >
+              로그아웃
+            </li>
           </ul>
         ) : null}
         {user.data ? (
