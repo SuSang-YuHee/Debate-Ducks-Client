dc6c1acd8735487e62c885be94c3f793bca55ff9
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-08-17 19:29:07 +0900

    [Add] 토론 진행 페이지 SSR 추가

--- pages/debateroom/[debateId].tsx
+++ pages/debateroom/[debateId].tsx
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
+import { GetServerSideProps } from "next";
+import { dehydrate, QueryClient } from "react-query";
 
+import { getDebate } from "../../api/debates";
 import { useGetUser } from "../../utils/queries/users";
 import { useGetDebate } from "../../utils/queries/debates";
 
@@ -45,3 +48,19 @@ export default function DebateroomPage() {
     />
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const debateId =
+    typeof context.params?.debateId === "string"
+      ? parseInt(context.params?.debateId)
+      : 0;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(["debates", `${debateId}`], () =>
+    getDebate(debateId),
+  );
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+    },
+  };
+};
