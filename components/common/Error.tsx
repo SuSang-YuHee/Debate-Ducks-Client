283eccd1ec05e46da988b8e32e293a2a1e083024
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-08-21 17:05:03 +0900

    [Add] 에러 페이지 추가

---
+++ components/common/Error.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import styles from "./Error.module.scss";
+
+export default function Error() {
+  const router = useRouter();
+
+  return (
+    <div className={`inner ${styles.outer}`}>
+      <div className={`${styles.box}`}>
+        <div className={styles.message_top}>{"SORRY"}</div>
+        <div className={styles.message_bottom}>
+          {"we couldn't find that page"}
+        </div>
+        <div
+          className={styles.btn}
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          {"Go to homepage"}
+        </div>
+        <Image
+          src={"./images/duckling.png"}
+          alt={"duckling"}
+          width={400}
+          height={400}
+          unoptimized={true}
+          priority={true}
+        />
+      </div>
+    </div>
+  );
+}
